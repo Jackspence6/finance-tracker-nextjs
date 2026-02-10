3e082e924eb5c0f7877b59ef22e6729e14f7f1c9
Jack Spence <jackspence.dev@gmail.com>
2026-02-10 21:49:17 +0200

    Adds icons to navbar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,6 +2,7 @@
 
 import React, { ReactNode } from "react";
 import Link from "next/link";
+import Image from "next/image";
 import { usePathname } from "next/navigation";
 
 type Props = {
@@ -9,15 +10,15 @@ type Props = {
 };
 
 export default function Layout({ children }: Props) {
-    // current path
+    // Current path
     const pathname = usePathname();
 
     const navItems = [
-        { label: "Overview", href: "/" },
-        { label: "Transactions", href: "/transactions" },
-        { label: "Budgets", href: "/budgets" },
-        { label: "Pots", href: "/pots" },
-        { label: "Recurring Bills", href: "/recurring" },
+        { label: "Overview", href: "/", icon: "/assets/images/icon-nav-overview.svg" },
+        { label: "Transactions", href: "/transactions", icon: "/assets/images/icon-nav-transactions.svg" },
+        { label: "Budgets", href: "/budgets", icon: "/assets/images/icon-nav-budgets.svg" },
+        { label: "Pots", href: "/pots", icon: "/assets/images/icon-nav-pots.svg" },
+        { label: "Recurring Bills", href: "/recurring", icon: "/assets/images/icon-nav-recurring-bills.svg" },
     ];
 
     return (
@@ -33,12 +34,16 @@ export default function Layout({ children }: Props) {
                             <Link
                                 key={item.href}
                                 href={item.href}
-                                className={`text-left font-semibold px-3 py-2 rounded hover:underline ${isActive
-                                    ? "bg-white text-black" // active link style
-                                    : "text-white"
+                                className={`flex items-center gap-3 font-semibold px-3 py-2 rounded hover:underline ${isActive ? "bg-white text-black" : "text-white"
                                     }`}
                             >
-                                {item.label}
+                                <Image
+                                    src={item.icon}
+                                    alt={`${item.label} icon`}
+                                    width={20}
+                                    height={20}
+                                />
+                                <span>{item.label}</span>
                             </Link>
                         );
                     })}
